55f3565d0b121db5342fb7d0d1376c5e196bf266
Batuhan <batuhan.tas@yahoo.com>
2023-08-08 18:01:37 +0300

    Gorevler Tamamlandi

--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,12 @@
+import { NOT_EKLE, NOT_SIL } from "./actions";
+
 const s10chLocalStorageKey = "s10ch";
 
 const baslangicDegerleri = {
   notlar: [
     {
       id: "75g1IyB8JLehAr0Lr5v3p",
-      date: "Fri Feb 03 2023 09:40:27 GMT+0300 (GMT+03:00)",
+      date: "Tue Aug 08 2023 16:22:00 GMT+0300 (GMT+03:00)",
       body: "Bugün hava çok güzel!|En iyi arkadaşımın en iyi arkadaşı olduğumu öğrendim :)|Kedim iyileşti!",
     },
   ],
@@ -21,9 +23,35 @@ function localStorageStateOku(key) {
 function baslangicNotlariniGetir(key) {
   const eskiNotlar = localStorage.getItem(key);
 
-  if (eskiNotlar) {
+  if (eskiNotlar !== null) {
     return localStorageStateOku(key);
   } else {
-    return baslangicDegerleri
+    return baslangicDegerleri;
   }
 }
+
+function reducer(
+  state = baslangicNotlariniGetir(s10chLocalStorageKey),
+  action
+) {
+  switch (action.type) {
+    case NOT_EKLE:
+      const yeniNotlar = {
+        ...state,
+        notlar: [action.payload, ...state.notlar],
+      };
+      localStorageStateYaz(s10chLocalStorageKey, yeniNotlar);
+      return yeniNotlar;
+    case NOT_SIL:
+      const silinmisNotlar = {
+        ...state,
+        notlar: state.notlar.filter((not) => not.id !== action.payload),
+      };
+      localStorageStateYaz(s10chLocalStorageKey, silinmisNotlar);
+      return silinmisNotlar;
+    default:
+      return state;
+  }
+}
+
+export default reducer;
